2b3c62b931b198c48b4489acfdd36435ae9e67c6
Tony Baker <this.tonybaker@gmail.com>
2019-08-22 20:44:38 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'users#welcome'
+  get '/signin' => 'users#signin', as: :signin
+  post '/login' => 'users#login', as: :login
+  get '/logout' => 'users#logout', as: :logout
+  resources :users, only: [:new, :show, :create, :edit, :update]
+  resources :attractions
+  post '/attractions/ride' => 'attractions#ride'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
