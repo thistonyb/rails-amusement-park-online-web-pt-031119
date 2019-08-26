260a0f9e37259a5a10b34e8eac7295e1715e9ef0
Tony Baker <this.tonybaker@gmail.com>
2019-08-26 22:22:52 +0000

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class RidesController < ApplicationController
     def create
-        @ride = Ride.create(user_id: current_user.id,attraction_id: params[:attraction_id])
+        @ride = Ride.create(user_id: current_user.id, attraction_id: params[:attraction_id])
         @message = @ride.take_ride
         redirect_to user_path(@ride.user, :message => @message)
     end
